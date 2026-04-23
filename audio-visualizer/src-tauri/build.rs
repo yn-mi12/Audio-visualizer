cfbda9af1a3ed682d45091813613d0901fee4b39
yn-mi12 <yana.mihaylova04@gmail.com>
2026-04-23 15:32:21 +0200

    set up dependencies

--- audio-visualizer/src-tauri/build.rs
+++ audio-visualizer/src-tauri/build.rs
@@ -1,3 +1,14 @@
 fn main() {
-    tauri_build::build()
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    if target_os == "linux" {
+        #[cfg(target_os = "linux")]
+        {
+            if pkg_config::probe_library("libpipewire-0.3").is_err() {
+                eprintln!("Missing libpipewire-0.3");
+            }
+        }
+    }
+
+    tauri_build::build();
 }
